f4d0eaae415c4733b21d4011812102705e7dbbd4
Andre <andre.agrippa@dcmail.ca>
2020-12-17 20:50:31 -0500

    Simple BMI calculator, review later

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -36,19 +36,21 @@ public class Main extends Application
         Font font = Font.font("Consolas",20);
 
         //Label for height, weight, BMI and BMI Value
-        Label lblHeight = new Label("My Height");
+        Label lblHeight = new Label("My Height (in):");
         lblHeight.setFont(font);
-        Label lblWeight = new Label("My Weight");
+        Label lblWeight = new Label("My Weight (lb):");
         lblWeight.setFont(font);
         Label lblBMI = new Label("BMI:");
         lblBMI.setFont(font);
+
         Label lblBMIValue = new Label();
+        lblBMIValue.setFont(font);
 
         //Height and weight text input
         TextField txtHeight = new TextField();
         TextField txtWeight = new TextField();
 
-        Button btnCalculate = new Button("Calculate");
+        Button btnCalculate = new Button("Calculate BMI");
         btnCalculate.setFont(font);
 
 
@@ -60,22 +62,25 @@ public class Main extends Application
         hBoxHeight.setAlignment(Pos.CENTER);
         hBoxWeight.setAlignment(Pos.CENTER);
 
-        gridPane.add(lblHeight,1,1);
-        gridPane.add(txtHeight,1,5);
+        //Add objects to grid pane
 
+        //Height
+        gridPane.add(lblHeight,1,1);
+        gridPane.add(txtHeight,1,2);
+
+        //Weight
         gridPane.add(lblWeight,1,10);
         gridPane.add(txtWeight,1,20);
 
 
+        //Button, image, BMI label, and BMI value
         gridPane.add(btnCalculate, 1,40);
         gridPane.add(bmiImageView,1,60);
         gridPane.add(lblBMI,1,80);
         gridPane.add(lblBMIValue,1,100);
 
 
-
-        //gridPane.add(label, 1,1);
-        Scene scene = new Scene(gridPane, 320, 480);
+        Scene scene = new Scene(gridPane, 700, 500);
         primaryStage.setScene(scene);
         primaryStage.show();
 
@@ -85,8 +90,30 @@ public class Main extends Application
                 double inputWeight = 0;
                 double inputHeight = 0;
                 double BMI = 0;
-                lblBMIValue.setText("CLICKED FAM");
-                BMI = (inputWeight * 703) / (inputHeight * inputHeight);
+
+                try
+                {
+                    inputHeight = Double.parseDouble(txtHeight.getText());
+                    inputWeight = Double.parseDouble(txtWeight.getText());
+
+                    if(inputHeight < 1 || inputWeight < 1)
+                    {
+                        throw new NumberFormatException("Cannot be less than 0.");
+                    }
+                    else
+                    {
+                        BMI = (inputWeight * 703) / (inputHeight * inputHeight);
+                        lblBMIValue.setText((String.format("%1$,.1f",BMI)));
+                    }
+
+                }
+                catch (NumberFormatException e)
+                {
+                    lblBMIValue.setText("Error: " + e);
+                }
+
+
+
             }
         });
 
